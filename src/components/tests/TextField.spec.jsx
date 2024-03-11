eccfe7e5a5639d6096914f15596b5806e1450ba7
seob171 <seob9717@gmail.com>
2024-03-12 01:32:09 +0900

    2024-03-12 2.3 chapter

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -4,6 +4,36 @@ import React from 'react';
 import TextField from '@/components/TextField';
 import render from '@/utils/test/render';
 
-it('sample test.', async () => {
-  expect(1).toBe(1);
+beforeEach(async () => {
+  await render(<TextField className={'my-class'} />);
+});
+
+it('className prop으로 설정한 css class가 적용된다.', async () => {
+  // await render(<TextField className={'my-class'} />);
+
+  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+  screen.debug();
+
+  expect(textInput).toHaveClass('my-class');
+});
+
+describe('placeholder', () => {
+  it('기본 placeholder "텍스트를 입력해 주세요."가 노출된다.', async () => {
+    const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+    screen.debug();
+
+    expect(textInput).toBeInTheDocument();
+  });
+
+  it('placeholder prop에 따라 placeholder가 변경된다.', async () => {
+    await render(<TextField placeholder={'상품명을 입력해 주세요.'} />);
+
+    const textInput = screen.getByPlaceholderText('상품명을 입력해 주세요.');
+
+    screen.debug();
+
+    expect(textInput).toBeInTheDocument();
+  });
 });
